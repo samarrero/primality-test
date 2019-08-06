0a90d59902f5bbeb60d540f4dd664c7531f184a4
Samuel Marrero <helicopter1339@gmail.com>
2019-08-06 13:39:17 -0400

    Update bruh.py
    improved algorithm accuracy and speed.
    issue: still inaccurate for large numbers

--- primality test/bruh.py
+++ primality test/bruh.py
@@ -3,17 +3,31 @@
 @author: samuel
 """
 
-minNum = 4
+import math
 
 print("greetings! i will go through a range of natural numbers and declare their primality!")
 
+minNum = 4
 maxNum = int(input("enter max pl0xXz!!!11: "))
 
 for currentNum in range(minNum,maxNum+1): #go thru a list of ints
     
     #print(currentNum,'currently. ', end='') #debug current int
     numFac = 0
+    isPrime = bool(True)
+    
     if currentNum % 2 == 0 or currentNum % 3 == 0:
-        print(currentNum,'is composite.')
+        isPrime = False
     else:
-        print(currentNum,'is prime.')
+        for k in range(0,int(math.sqrt(currentNum))):
+            if currentNum % 6*k+1 == 0:
+                isPrime = False
+            else:
+                isPrime = True       
+            
+    if isPrime is False:
+        print(currentNum,'is composite.')
+    elif isPrime is True:
+       print(currentNum,'is prime.') 
+       
+       
