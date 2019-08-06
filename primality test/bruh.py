97b0a5bc8200627f3b6703c27318187ac61da983
Samuel Marrero <helicopter1339@gmail.com>
2019-08-06 12:57:16 -0400

    Update bruh.py
    changed algorithm to check if number is divisible by 2 or 3, rather than brute force potential factors.

--- primality test/bruh.py
+++ primality test/bruh.py
@@ -3,27 +3,17 @@
 @author: samuel
 """
 
-minNum = 2
+minNum = 4
 
-print ("greetings! i will go through a range of natural numbers and declare their primality!")
+print("greetings! i will go through a range of natural numbers and declare their primality!")
 
-    
-maxNum = int(input("enter max pls: "))
+maxNum = int(input("enter max pl0xXz!!!11: "))
 
 for currentNum in range(minNum,maxNum+1): #go thru a list of ints
     
     #print(currentNum,'currently. ', end='') #debug current int
     numFac = 0
- 
-    for potFac in range(minNum,currentNum): #seek potential factors
-     
-        #print ('testing if',potFac,'is a factor. ',end='') #debug current potential factor
-        if currentNum % potFac == 0:
-            #print (potFac, end=' is a factor. ')
-            numFac += 1
-            break       
-    
-    if numFac == 0:
-     print(currentNum,'is prime.')
+    if currentNum % 2 == 0 or currentNum % 3 == 0:
+        print(currentNum,'is composite.')
     else:
-     print(currentNum,'is composite.')
+        print(currentNum,'is prime.')
